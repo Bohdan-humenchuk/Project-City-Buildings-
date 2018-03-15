6bc764ac50122aec252ddd7e2be4b4ad8b36dae3
Bohdan <bohdan.humenchuk"gmail.com>
2018-03-16 00:08:53 +0200

    added socket to client

--- app/CoordsGps2/app/src/main/java/com/example/a911/coordsgps2/Sender.java
+++ app/CoordsGps2/app/src/main/java/com/example/a911/coordsgps2/Sender.java
@@ -1,6 +1,30 @@
 package com.example.a911.coordsgps2;
+import java.io.*;
+import java.net.*;
 
 
-public class Sender {
-    
+public class Sender extends Thread{
+    public static void main(String args[])
+    {
+        try
+        {
+            // получаем сокет сервера
+            Socket s = new Socket("localhost", 25000);
+            InputStream instream;
+            OutputStream outstream;
+            instream = s.getInputStream();
+            outstream = s.getOutputStream();
+
+            // читаем ответ
+            byte buf[] = new byte[2048];
+            String r = instream.read(buf);
+            System.out.println(r)
+            outstream.write(buf);
+
+            // выводим ответ в консоль
+            System.out.println(data);
+        }
+        catch(Exception e)
+        {System.out.println("init error: "+e);} // вывод исключений
+    }
 }
