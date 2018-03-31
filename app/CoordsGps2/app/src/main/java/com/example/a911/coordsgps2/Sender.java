ace5c85ea81043db518120f85804ae691d123699
Bohdan <bohdan.humenchuk"gmail.com>
2018-03-31 22:53:30 +0300

    add changes to Sender.java

--- app/CoordsGps2/app/src/main/java/com/example/a911/coordsgps2/Sender.java
+++ app/CoordsGps2/app/src/main/java/com/example/a911/coordsgps2/Sender.java
@@ -9,25 +9,30 @@ public class Sender extends Thread{
     {
         try
         {
-            Socket s = new Socket("127.0.0.1", 25970);
+            Socket s = new Socket("77.120.204.6", 25970);
             InputStream instream;
             OutputStream outstream;
             instream = s.getInputStream();
             outstream = s.getOutputStream();
+            byte buf[] = new byte[64*1024];
 
             String longtitude = Double.toString(longtit)
             String latitude = Double.toString(latit)
             String angle = Double.toString(ang)
-            s.getOutputStream().write(longtitude.getBytes());
-            s.getOutputStream().write(latitude.getBytes());
-            s.getOutputStream().write(angle.getBytes());
 
+            s.getOutputStream().write(longtitude.getBytes());
+            String message=s.getInputStream().read(buf);
+            s.getOutputStream().write(latitude.getBytes());
+            String message=s.getInputStream().read(buf);
+            s.getOutputStream().write(angle.getBytes());
+            String message=s.getInputStream().read(buf);
    
-            byte buf[] = new byte[2048];
+            byte buf[] = new byte[64*1024];
             String r = instream.read(buf);
-            System.out.println(r);
+
         }
         catch(Exception e)
         {System.out.println("init error: "+e);}
+        return r;
     }
 }
